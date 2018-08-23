98b9b39f29b54205145ffcaa14b93f16ea130ce0
Anthony Oliveri <oliveri@us.ibm.com>
2018-08-23 15:10:28 -0500

    New XCode project

---
+++ Source/Supporting Files/RestKit.h
@@ -0,0 +1,19 @@
+//
+//  RestKit.h
+//  RestKit
+//
+//  Created by Anthony Oliveri on 8/23/18.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RestKit.
+FOUNDATION_EXPORT double RestKitVersionNumber;
+
+//! Project version string for RestKit.
+FOUNDATION_EXPORT const unsigned char RestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestKit/PublicHeader.h>
+
+
